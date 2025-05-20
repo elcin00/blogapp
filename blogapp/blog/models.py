73060854fa495e5ab8ddc7d55774eb2e05daac3c
elcin00 <informationtechnologies00@gmail.com>
2025-05-20 23:52:08 +0400

    category ve blog arasinda manytoone elaqesi yaradildi

--- blogapp/blog/models.py
+++ blogapp/blog/models.py
@@ -6,6 +6,17 @@ from ckeditor.fields import RichTextField
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to="blogs")
@@ -13,6 +24,7 @@ class Blog(models.Model):
     is_active = models.BooleanField(default=False)
     is_home = models.BooleanField(default=False)
     slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
+    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title}"
@@ -21,13 +33,3 @@ class Blog(models.Model):
         self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
 
-class Category(models.Model):
-    name = models.CharField(max_length=150)
-    slug = models.SlugField(null=False, blank=True, unique=True, db_index=True, editable=False)
-
-    def __str__(self):
-        return self.name
-    
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        return super().save(*args, **kwargs)
